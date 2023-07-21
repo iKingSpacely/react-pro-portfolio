fe69a667b43930596cfecb96663cad0a9a6a480f
Spacely <accolin89@gmail.c>
2023-07-20 19:59:12 -0500

    working through routing, getting correct code strcuture in all files, and started working on the navbar css and layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
-import React from 'react';
+import React from 'react'
 
-import Header from './components/pages/Header';
-import Nav from './components/pages/Nav';
-import PageContent from './components/pages/PageContent';
-import Footer from './components/pages/Footer';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Contact from './components/pages/Contact';
 import About from './components/pages/About';
 import Resume from './components/pages/Resume';
+import Home from './components/pages/Home';
+import Nav from './components/pages/Nav';
+import Projects from './components/pages/Projects'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 
 
-export default App;
+export default function App() {
+    return (
+        <Router>
+            <div>
+                <Nav />
+                <Routes>
+                    <Route
+                        path="/"
+                        element={<Home />}
+                    />
+                    <Route
+                        path="/about"
+                        element={<About />}
+                    />
+                    <Route
+                        path="/projects"
+                        element={<Projects />}
+                    />
+                    <Route
+                        path="/contact"
+                        element={<Contact />}
+                    />
+                    <Route
+                        path="/resume"
+                        element={<Resume />}
+                    />
+                </Routes>
+            </div>
+        </Router>
+    )
+};
